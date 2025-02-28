1b25b5fcdaf069e9a5d6e08dc8e123971b2d0051
Ruilx <ruilxalxa@qq.com>
2025-02-28 19:09:24 +0800

    update

--- src/app/module1/con1/action1.py
+++ src/app/module1/con1/action1.py
@@ -2,6 +2,15 @@
 from src.framework.dry.base.action.item_action import ItemAction
 
 
+# a function to calculate the datetime with giving UTC timestamp.
+def calculate_datetime(timestamp: int):
+    import datetime
+    return datetime.datetime.utcfromtimestamp(timestamp)
+
+# a function to calculate the datetime with giving UTC timestamp but not to use python datetime libraries.
+
+
+
 class Action1(ItemAction):
     def action(self):
         method = self.request.method
