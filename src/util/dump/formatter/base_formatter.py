f54612aa131fafe07f2d104fb5b95687bbd337da
Ruilx <799382072@qq.com>
2025-12-08 04:12:28 +0800

    update

--- src/util/dump/formatter/base_formatter.py
+++ src/util/dump/formatter/base_formatter.py
@@ -3,4 +3,9 @@ import abc
 
 
 class Formatter(metaclass=abc.ABCMeta):
-    ...
+    def __init__(self, root: List[Node]):
+        self.root = root
+
+    @abc.abstractmethod
+    def render(self) -> str:
+        raise NotImplementedError
--- src/util/dump/node.py
+++ src/util/dump/node.py
@@ -1,13 +1,34 @@
 # -*- coding: utf-8 -*-
+import enum
+from typing import Any, Self, Iterator
+
+from black.nodes import NodeType
 
 from .formatter.base_formatter import Formatter
 
+
+'''
+<__main__.A obj  @=0x1b547355730 __sizeof__=16>
+|TITLE     |TYPE| ATTR1         |ATTR2        |
+
+ member3 = <str   @=0x1b547c09a70 __sizeof__=48 __len__=7> ABCDEFG
+|KEY    |  |TITLE| ATTR1         | ATTR2       | ATTR3    | TEXT
+'''
+
 class Node(object):
+
+    class NodeType(enum.Enum):
+        NodeType_Root = enum.auto()
+        NodeType_Value = enum.auto()
+
     def __init__(self, title: str):
-        self.title: str = title
-        self.kv_sep: str = " = "
+        self.title = ""
+        self.key = ""
+        self.type: NodeType = NodeType.NodeType_Root
         self.attrs: dict[str, Any] = {}
+        self.text = ""
         self.children: list[Node] = []
+        self.set_title(title)
 
     def set_title(self, title: str) -> Self:
         self.title = title
@@ -16,12 +37,12 @@ class Node(object):
     def get_title(self):
         return self.title
 
-    def set_kv_sep(self, kv_sep: str) -> Self:
-        self.kv_sep = kv_sep
+    def set_key(self, key: str) -> Self:
+        self.key = key
         return self
 
-    def get_kv_sep(self):
-        return self.kv_sep
+    def get_key(self):
+        return self.key
 
     def set_attr(self, key: str, value: Any) -> Self:
         self.attrs[key] = value
